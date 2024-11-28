a6b9b4b917d9a6586d1fe3bbd8a7b48abee5e58e
Jitesh <jiteshgopale26@gmail.com>
2024-11-28 15:44:52 +0530

    task vv

--- Backend/db.js
+++ Backend/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose
-    .connect("mongodb://localhost:27019/my-app")
+    .connect("mongodb+srv://jiteshgopale26:0nef0TygG3zYb0La@cluster0.rtvbi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     .then(() => console.log("DB Connected"))
     .catch((err) => console.error("DB Connection Error: ", err));
 };
